0593de7f33e3c5256f980dd04e3c5c58eaba196d
ayysa <ayusafitri2789@gmail.com>
2025-07-31 22:43:19 +0700

    string

--- bin/conversion.dart
+++ bin/conversion.dart
@@ -1,9 +1,20 @@
 void main() {
-    var inputString = '1000';
-    var inputInt = int.parse(inputString);
-    var inputDouble = double.parse(inputString);
+  var inputString = '1000';
+  var inputInt = int.parse(inputString);
+  var inputDouble = double.parse(inputString);
 
-    print(inputString);
-    print(inputInt);
-    print(inputDouble);
+  print(inputString);
+  print(inputInt);
+  print(inputDouble);
+
+  var intToDouble = inputInt.toDouble(); // nama diganti dari inputDouble ke intToDouble
+  var doubleToInt = intToDouble.toInt(); // pakai intToDouble, bukan inputDouble
+
+  var intToString = inputInt.toString();
+  var doubleToString = intToDouble.toString();
+
+  print(intToDouble);
+  print(doubleToInt);
+  print(intToString);
+  print(doubleToString);
 }
