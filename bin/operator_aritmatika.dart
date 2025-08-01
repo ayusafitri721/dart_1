b985fd07549332143ecd5016cc71b5b3506bfb84
ayysa <ayusafitri2789@gmail.com>
2025-08-01 08:51:11 +0700

    boolean perbandingan

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,17 @@
+void main() {
+    var first = 10;
+    var second = 3;
+// ditambah
+    print(first + second);
+    // dikurang
+    print(first - second);
+    // dikali
+    print(first * second);
+    // pembagian hasil module
+    print(first / second);
+    // pembagian hasil int
+    print(first ~/ second);
+    // sisa bagi
+    print(first % second);
+
+}
